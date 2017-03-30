0c4c8f4e098f9c1f747811c422541c7fd0233424
WhosJohnGault <Bravesboy1116@gmail.com>
2017-03-30 10:06:58 -0700

    Update main.cpp

--- Lab/lab32817/GameofCraps_VIII/main.cpp
+++ Lab/lab32817/GameofCraps_VIII/main.cpp
@@ -17,68 +17,80 @@ using namespace std;
 
 //Global Constants Only, No Global Variables
 //Like PI, e, Gravity, or conversions
-const float PERCENT=1.00f;
+const float PERCENT=100.0f;
 //Function Prototypes Here
 
 //Program Execution Begins Here
 int main(int argc, char** argv) {
-//Declare and initialize Variables Here
-srand(static_cast<unsigned int>(time(0)));        
-ifstream in;    
-ofstream out;
-int nGames,wins=0,losses=0;
-int mxThrw=0,numThrw=0,lmGames;
-    
+	//Set the random number seed
+	srand(static_cast<unsigned int>(time(0)));	
 
-//declare all variables here 2
-in.open("GameInfo.dat");
-out.open("GameStats.dat");
-while (in>>nGames);
-nGames=nGames>lmGames?lmGames:nGames;
+	//Declare and initialize Variables Here
+	ifstream in;    
+	ofstream out;
+	int nGames,wins=0,losses=0;
+	int mxThrw=0,numThrw=0,lmGames;    
 
-//Loop and throw the dice
-for(int game=1;game<=nGames;game++){
+	//Initialize variables here 
+	in.open("GameInfo.dat");
+	while (in>>nGames);
+	
+	//Play the game
+	for(int game=1;game<=nGames;game++){
+	//Throw dice and sum
+	int gmThrw=1;
 	char die1=rand()%6+1;//[1,6]
 	char die2=rand()%6+1;//[1,6]	
 	char sum1=die1+die2;
-        if (sum1==7||sum1==11)wins++;
-        else if (sum1==2||sum1==3||sum1==12)losses++;
-        else{
-            //loop until a 7 or previous sum is thrown
-            bool thrwAgn=true;
-            do{
-                //Throw the dice again
-                char die1=rand()%6+1;//[1,6]
+	//Determine wins and losses
+	switch(sum){
+		case 7:
+		case 11:wins++;break;
+		case  2:
+		case  3:
+		case 12:losses++;break;
+		default:{
+		    //Loop until a 7 or previous sum is thrown
+		    bool thrwAgn=true;
+		    do{	
+		//Throw and dice again
+		char die1=rand()%6+1;//[1,6]
                 char die2=rand()%6+1;//[1,6]
                 char sum2=die1+die2;
-                if(sum2==7){
+		gmThrw++;
+		if(sum2==7){
                     losses++;
                     thrwAgn=false;
-                }else if (sum1==sum2){
+		 }else if (sum1==sum2){
                     wins++;
                     thrwAgn=false;
-                
-                }
+		  }
             }while(thrwAgn);//do-while
         }
-
-numThrw=gmThrw;
-if(mxThrw<gmThrw)mxThrw=gmThrw;// Independent if
+	}
+	numThrw+=gmThrw;
+	if(mxThrw<gmThrw)mxThrw=gmThrw;// Independent if
 }
-cout<<"Total number of Games = "<<nGames<<endl;
-cout<<"Number of Games won   = "<<wins<<end;
-cout<<"Number of Games lost  = "<<losses<<endl;
-cout<<"Total number of Games = "<<wins+losses<<endl;
-cout<<"Percentage wins       =  "
-        <<static_cast<float>(wins)/nGames*PERCENT<<"%"<<endl
-cout<<"Percentage losses     =  "
-        <<static_cast<float>(losses)/nGames*PERCENT<<"%"<<endl;
-//Input or initialize values Here
+	//Output the transformed data
+	cout<<fixed<<setprecision(2)<<showpoint;
+	cout<<"Total number of Games = "<<nGames<<endl;
+	cout<<"Number of Games won   = "<<wins<<end;
+	cout<<"Number of Games lost  = "<<losses<<endl;
+	cout<<"Total number of Games = "<<wins+losses<<endl;
+	cout<<"Percentage wins       =  "
+        	<<static_cast<float>(wins)/nGames*PERCENT<<"%"<<endl
+	cout<<"Percentage losses     =  "
+       		<<static_cast<float>(losses)/nGames*PERCENT<<"%"<<endl;
+	cout<<"Maximum number of throws in a game = "<<mxThrw<<endl;
+	cout<<"Average throw per game="<<static_cast<float>(numThrw)/nGames<<endl;
+	
+		
+out.open("GameStats.dat");
 
-//Process/Calculations Here
+nGames=nGames>lmGames?lmGames:nGames;
 
-//Output Located Here
-    //Exit
+
+    //Exit stage right!
 in.close()
     return 0;
 }
