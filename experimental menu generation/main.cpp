c992ebc095fc23dfdb1db0d4d6b93eb07f90d985
rcc <rcc>
2017-04-06 11:00:11 -0700

    Signed-off-by: rcc <rcc>

---
+++ experimental menu generation/main.cpp
@@ -0,0 +1,46 @@
+/* 
+ * File:   main.cpp
+ * Author: Dr. Mark E. Lehr
+ * Created on July 19, 2016, 9:07 AM
+ * Purpose:  Hello World Template
+ */
+
+//System Libraries Here
+#include <iostream>
+using namespace std;
+
+//User Libraries Here
+
+//Global Constants Only, No Global Variables
+//Like PI, e, Gravity, or conversions
+
+//Function Prototypes Here
+
+//Program Execution Begins Here
+int main(int argc, char** argv) {
+    //Declare all Variables Here
+    char prob,menuip;
+for (int i=1; i>=1,i<=9;i++){
+    int prob=i;
+    cout<<"please enter "<<i<<" for the desired menu selection for problem "<<prob<<endl;
+    switch (prob){
+        case '1':cout<<"Summation problem";
+    }
+}
+cin>>menuip;
+if (menuip=1){
+    cout<<"you have selected problem 1"<<endl;
+}
+
+
+    //Input or initialize values Here
+    
+    //Process/Calculations Here
+    
+    //Output Located Here
+   
+
+    //Exit
+    return 0;
+}
+
