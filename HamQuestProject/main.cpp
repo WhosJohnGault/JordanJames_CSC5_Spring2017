43ed77d997766c2595f8410a1fa186e9386efbbe
rcc <rcc>
2017-04-17 12:18:59 -0700

    Signed-off-by: rcc <rcc>

--- HamQuestProject/main.cpp
+++ HamQuestProject/main.cpp
@@ -16,57 +16,63 @@
 using namespace std;
 
 //User Libraries Here
-
+//Function Prototypes
+void combat(char,int );
 //Program Execution Begins Here
 int main(int argc, char** argv) {
-int combat
-int enemyhp    
-char z; //The Best offense is a great offense, Fight!
-char x; //"Mama raised a coward", Run!
-char c; //"I always wanted to be a rock", Guard!
+char react;
+int enemyhp=2; 
 char enter;
     
 //prologue/ character introduction sequence
-    cout<<"Thank you for choosing to play Ham Quest demo version 0.01"<<endl; //mandatory intro/thankyou sequence
+    cout<<"Thank you for choosing to play Ham Quest demo version 0.01"<<endl; //mandatory intro/thank you sequence
     cout<<"The goal of this game is to liberate your fellow pigs from the "
-            "slaughter house, when you see ... you press  a charecter on the keyboard followed by the enter key
-        to progess the dialogue"<<endl;
+            "slaughter house, when you see ... you press  a character on the keyboard followed by the enter key"
+        "to progress the dialogue"<<endl;
     cout<<"before we begin what is your name?"<<endl; //this will cause a user requested input to prompt
     char input[100]; //name input for your character, can handle spaces
     cin.getline(input,sizeof(input)); //this is what allows the user input name to handle spaces
-    cout<<"Oh you name is "<<input<<"?"<<endl; //input will act as the players name in dialoge and possible combat menus
+    cout<<"Oh you name is "<<input<<"?"<<endl; //input will act as the players name in dialogue and possible combat menus in future versions
     cout<<"Good luck with saving your fellow pigs "<<input<<endl;
     cout<<"you lie in a dim room, dreaming the dreams of pigs when suddenly...";
     cin>>enter;
-    cout<<"Hearing a voice, you slowly awaken you hear a voice saying wakey wakey ..."<<endl;
+    cout<<"Hearing a voice, you slowly awaken you hear a voice saying wakey wakey pig shit..."<<endl;
     cin>>enter;
        cout<<" when you feel a sudden kick to your ribs "<<endl;
     cout<<"The kick is a sudden sting and you can't help but wince from the feeling"<<endl;
+    cin>>enter;
+    cout<<"How do you react? "<<input<<"(you can input z to attack, x to run away, c to guard"<<endl;
     
-    cout<<"How do you react? "<<input"(you can input z to attack, x to run away, c to gaurd";
-    do while(enemyhp>0)
-    switch(react){
-    'z':cout<<"You muster all the strength in your piggy body to hit your assailant, you dealt no dmg
-        cout<<"maybe you should try again?"
-        combat+1;
-        break;
-            
-    'x':cout<<"Looking or a place to hide, you realize you are in an incredibly dark room<<endl;
-        cout<<"You decide to run around in circles"<<endl;
-        break;
-            
-    'c':cout<<"You tense for an incomming blow
-        break;
-            
-        default:cout<<"Come on pork for brains, you are going to die!"<<endl;
-        break;
-            }
-    '        
-
+    //first combat encounter
+    combat(react,enemyhp); //variables used in this prototype
  
     return 0;
 }
-
+void combat(char mander,int enemyhp){ //a modular encounter, likely the only encounter
+ do{ 
+    cin>>mander;
+    switch(mander){
+    case 'z':cout<<"You muster all the strength in your piggy body to hit your assailant, you didn't leave any noticeable damage, try again!"<<endl;
+    cout<<"(you can input z to attack, x to run away, c to guard"<<endl;
+        enemyhp--;
+        break;
+            
+    case 'x':cout<<"Looking or a place to hide, you realize you are in an incredibly dark room"<<
+                   "You decide to run around in circles!"<<endl;
+    cout<<"(you can input z to attack, x to run away, c to guard"<<endl;
+        break;
+            
+    case 'c':cout<<"You tense for an incoming blow, but seriously why would you want to get hit?"<<endl;
+    cout<<"(you can input z to attack, x to run away, c to guard"<<endl;
+        break;
+            
+        default:cout<<"Come on pork for brains, you are going to die!"<<endl;
+        cout<<"(you can input z to attack, x to run away, c to guard"<<endl;
+        break;
+            }        
+    }while(enemyhp>0);    
+    
+}
     
 
 
