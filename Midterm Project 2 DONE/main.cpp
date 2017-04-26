ba72f6ec297e435fbab8e04a824a3daea09d7b6e
WhosJohnGault <Bravesboy1116@gmail.com>
2017-04-25 18:54:58 -0700

    Signed-off-by: WhosJohnGault <Bravesboy1116@gmail.com>

---
+++ Midterm Project 2 DONE/main.cpp
@@ -0,0 +1,39 @@
+/* 
+ * File:   main.cpp
+ * Author: James Jordan
+ * Created on April 23, 2017, 2:02 PM
+ * Purpose:number to asterisk conversion
+ */
+
+//System Libraries Here
+#include <iostream>
+using namespace std;
+
+//User Libraries Here
+
+//Global Constants Only, No Global Variables
+//Like PI, e, Gravity, or conversions
+
+//Function Prototypes Here
+
+//Program Execution Begins Here
+int main(int argc, char** argv) {
+//Declare and initialize Variables Here
+ int a; // user input which will limit the for loop
+        cout<<"This program will output asterisks equal to your numerical input"<<endl;
+        cout<<"The program will not output a non positive, or non numerical character"<<endl;
+        cout << "Enter a number"<<endl; 
+            cin>>a;
+
+    for(int i=0; i<a; i++){ //for loop to spam * as long as it is less than the user input
+        cout << "*";
+        }
+        cout<<endl;
+    
+//Process/Calculations Here
+
+//Output Located Here
+    //Exit
+    return 0;
+}
+
